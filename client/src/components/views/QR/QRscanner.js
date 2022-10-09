6d680c8e320fa6447bfbf54078e49b629f957326
redb0ng <cww21@naver.com>
2022-10-10 00:57:15 +0900

    QR추가

--- client/src/components/views/QR/QRscanner.js
+++ client/src/components/views/QR/QRscanner.js
@@ -3,9 +3,10 @@ import { Fab, TextareaAutosize } from "@material-ui/core";
 import { ArrowBack } from "@material-ui/icons";
 import { Link } from "react-router-dom";
 import QrScan from "react-qr-reader";
+import "../QR/qrGen.css";
 
 function QRscanner() {
-  const [qrscan, setQrscan] = useState("No result");
+  const [qrscan, setQrscan] = useState("");
   const handleScan = (data) => {
     if (data) {
       setQrscan(data);
@@ -17,30 +18,36 @@ function QRscanner() {
 
   return (
     <div>
-      <Link to="/">
-        <Fab style={{ marginRight: 10 }} color="primary">
-          <ArrowBack />
-        </Fab>
-      </Link>
-      <span>QR Scanner</span>
-
-      <center>
-        <div style={{ marginTop: 30 }}>
-          <QrScan
-            delay={300}
-            onError={handleError}
-            onScan={handleScan}
-            style={{ height: 240, width: 320 }}
+      <div className="headerQr">
+        <Link to="/">
+          <Fab id="btnBack">
+            <ArrowBack fontSize="large" />
+          </Fab>
+        </Link>
+        <div className="top">QR Scanner</div>
+      </div>
+      <hr className="hr" />
+      <div className="main2">
+        <QrScan
+          delay={300}
+          onError={handleError}
+          onScan={handleScan}
+          style={{ height: 240, width: 400, marginTop: 50 }}
+        />
+        <div className="textAreaIn">
+          <TextareaAutosize
+            style={{
+              fontSize: 20,
+              width: 320,
+              height: 100,
+              marginTop: 200,
+            }}
+            rowsMax={4}
+            defaultValue={qrscan}
+            value={qrscan}
           />
         </div>
-      </center>
-
-      <TextareaAutosize
-        style={{ fontSize: 18, width: 320, height: 100, marginTop: 100 }}
-        rowsMax={4}
-        defaultValue={qrscan}
-        value={qrscan}
-      />
+      </div>
     </div>
   );
 }
